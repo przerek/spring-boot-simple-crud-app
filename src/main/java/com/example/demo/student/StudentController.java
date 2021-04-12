386d7ddf851bc6c36e06db803918be7184d80e54
przerek <przerek@gmail.com>
2021-04-12 17:30:49 +0200

    make it better

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,106 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@RestController //tu serv endpoints
+@Controller
+@RequestMapping(path = "/test")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+
+
+
+
+    @ResponseBody
+    @GetMapping("/accountsjson")
+    public List<Student> getStudents() {
+
+        return studentService.getStudents();
+    }
+
+
+
+
+    @GetMapping("/index")
+    public String accounts(Model model) {
+        List<Student> listStudent = studentService.getStudents();
+        model.addAttribute("clientsList", listStudent);
+        return "index";
+    }
+
+
+
+//tak bylo
+//    @PostMapping
+//    public void registerNewStudent(@RequestBody Student student) {
+//        studentService.addNewStudent(student);
+//    }
+
+
+
+    @GetMapping("/dodaj")
+    public String ffform(Model model) {
+    model.addAttribute("student", new Student());
+    return "formularz";
+}
+    @PostMapping("/index")
+    public String greetingSubmit(@ModelAttribute Student student, Model model) {
+
+        studentService.addNewStudent(student);
+        List<Student> listStudent = studentService.getStudents();
+        model.addAttribute("clientsList", listStudent);
+        return "index";
+    }
+
+
+
+
+
+
+
+
+//tak bylo
+//    @DeleteMapping(path = "{studentId}")
+//    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+//        studentService.deleteStudent(studentId);
+//    }
+
+//    @DeleteMapping(path = "{studentId}")
+//    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+//        studentService.deleteStudent(studentId);
+//    }
+
+//////////////////////////////////////
+//    @RequestMapping(value = "/test/delete_user/{studentId}", method = RequestMethod.GET)
+
+@GetMapping("/delete/{studentId}")
+    public String handleDeleteUser(@PathVariable Long studentId,Model model) {
+        studentService.deleteStudent(studentId);
+
+        return "redirect:/test/index";
+    }
+
+
+
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
+
+}
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,71 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+private final StudentRepository studentRepository;
+
+@Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @ModelAttribute("getStudents")
+    public List<Student> getStudents(){
+    return studentRepository.findAll();
+
+
+//        return List.of(
+//                new Student(1L, "Mariam", "email", LocalDate.of(2000, Month.JANUARY,5),21),
+//                new Student(2L, "Henryk", "email", LocalDate.of(2000, Month.JANUARY,5),21)
+//        );
+
+    }
+
+    public void addNewStudent(Student student) {
+    Optional<Student> studentOptional =studentRepository.findStudentByEmail(student.getEmail());
+if(studentOptional.isPresent()){
+    throw new IllegalStateException("email taken");
+}
+studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+   boolean exist = studentRepository.existsById(studentId);
+
+   if(!exist){
+       throw new IllegalStateException("student does not exist");
+
+   }
+        studentRepository.deleteById(studentId);
+    }
+
+    public void updateStudent(Long studentId, String name, String email) {
+   Student student = studentRepository.findById(studentId).orElseThrow(()->new IllegalStateException("Student does not exist"));
+
+   if(name!= null && name.length()>0 && !Objects.equals(student.getName(),name))
+       student.setName(name);
+
+        if(email!= null && email.length()>0 && !Objects.equals(student.getEmail(),email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+
+            }
+            student.setEmail(email);
+        }
+
+    }
+
+
+
+}
